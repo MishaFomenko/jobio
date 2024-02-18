cd8513a83dba568ca0202b1c57397998e19d7318
Mykhailo Fomenko <misha.fomenko00@gmail.com>
2024-02-17 21:24:30 -0500

    protected the db uri

--- jobio-back/db/db.js
+++ jobio-back/db/db.js
@@ -3,7 +3,7 @@
 const { Client } = require('pg');
 
 const pool = new Client({
-    connectionString: 'postgres://bknoexmyzbmblz:be9bfec117febf9c6fa4d393883144a8396c5ed40637303e2f6f649b60f6235b@ec2-3-233-79-30.compute-1.amazonaws.com:5432/d6rmmrgvpjv0c8',
+    connectionString: process.env.DB_URI,
 
     ssl: {
         rejectUnauthorized: false
